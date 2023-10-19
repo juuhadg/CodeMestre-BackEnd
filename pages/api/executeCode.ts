733180d49eecbfa04f69702d161bfb8d51897593
juuhadg <juku.jj@gmail.com>
2023-10-19 14:23:36 -0300

    Endpoint de Solução de Exercícios, rodando e validando código

---
+++ pages/api/executeCode.ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import uploadCodigoJudge0 from '../../services/uploadCodigoJudge0'
+import { conectarBancoDeDados } from '../../middlewares/conectarBancoDeDados';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import { validarToken } from '../../middlewares/validarToken';
+
+
+
+
+
+const executeCode = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  if (req.method === 'POST') {
+
+
+    const { userId } = req.query
+    const usuario = await UsuarioModel.findById(userId);
+
+    if (!usuario) {
+      return res.status(400).json("Usuario Nao Encontrado")
+    }
+
+    const resposta = await uploadCodigoJudge0(req)
+
+    if (!resposta) {
+      return res.status(500).json("Erro ao Executar o código Internamente")
+    }
+
+    if (resposta.status.description === "Accepted") {
+
+      if (resposta.stdout != null) {
+
+        const respostaBuffer = Buffer.from(resposta.stdout, 'base64');
+        const respostaDecodificada = respostaBuffer.toString('utf-8');
+
+        if (respostaDecodificada.trim() === req.body.respostaEsperada) {
+
+          usuario.xp += 200;
+          usuario.problemasResolvidos += 1;
+
+          if (usuario.xp >= 500) {
+            usuario.xp -= 500;
+            usuario.level += 1;
+          }
+
+          await UsuarioModel.findByIdAndUpdate({ _id: usuario._id }, usuario);
+          return res.status(200).json("Resposta Correta, Problema Concluído com sucesso!")
+        }
+        else {
+          return res.status(200).json(`Código sem Erros, mas Resposta errada, resposta esperada : ${req.body.respostaEsperada} , resposta recebida : ${respostaDecodificada.trim()}`)
+        }
+      }
+    }
+    else {
+      return res.status(200).json("Resposta Incorreta, erro : " + resposta.status.description)
+    }
+
+  }
+
+  return res.status(400).json("Método Informado Inválido")
+
+}
+
+export default validarToken(conectarBancoDeDados(executeCode));
+
+
