786e79c462fa68b485bc6aeb6bfb5b554e1e5077
juuhadg <juku.jj@gmail.com>
2023-10-24 19:19:52 -0300

    endpoint ranking

--- models/UsuarioModel.ts
+++ models/UsuarioModel.ts
@@ -14,4 +14,4 @@ const UsuarioSchema = new Schema({
 
 })
 
-export const UsuarioModel =(mongoose.models.usuarios || mongoose.model('usuarios', UsuarioSchema)) ;
+export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', UsuarioSchema)) ;
