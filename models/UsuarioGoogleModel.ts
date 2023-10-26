aab9562b524bea390053bd2dbb780f047d23d25d
juuhadg <juku.jj@gmail.com>
2023-10-26 11:44:10 -0300

    Usuario Google Model Criado

---
+++ models/UsuarioGoogleModel.ts
@@ -0,0 +1,18 @@
+import mongoose, {Schema} from "mongoose";
+const UsuarioGoogleSchema = new Schema({
+    googleId: {type:String,required:true},
+    nome:{type: String, required : true},
+    email: {type : String, required : true},
+    avatar: {type : String, required : false},
+    usuarioDoGoogle: {type: Boolean, default:true},
+    xp: {type: Number, default: 0},
+    level: {type : Number, default: 1},
+    NumeroDeproblemasResolvidos: {type : Number, default: 0},
+    problemasResolvidosPorLinguagem: {type:Object,default: {javascript : 0,python: 0,csharp:0}},
+    problemasResolvidos: {type: Array, default: [], required: true},
+    missaoDiaria: {type:Object,required:false}
+
+
+})
+
+export const UsuarioGoogleModel =(mongoose.models.usuarios || mongoose.model('usuarios', UsuarioGoogleSchema)) ;
