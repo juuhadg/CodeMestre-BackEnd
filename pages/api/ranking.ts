3f1ff6b3877d11779f51ea2e85525ffb80f1c874
juuhadg <juku.jj@gmail.com>
2023-11-24 21:49:58 -0300

    endpoint de Ranking consertado

--- pages/api/ranking.ts
+++ pages/api/ranking.ts
@@ -6,25 +6,25 @@ import { politicaCORS } from "../../middlewares/CORS";
 const rankingEnpoint = async (req:NextApiRequest, res: NextApiResponse) => {
    
 
-        if(req.method === 'GET') {
+        if(req.method != 'GET') return res.status(400).json("Método De Request Inválido");
         
-           const usuarios = await UsuarioModel.find()
-           if(!usuarios) {
-            return res.status(400).json('usuarios nao encontrados')
-           }
-           const usuariosOrdenados = usuarios.sort(user => user.level)
-           const ranking = usuariosOrdenados.map((user, index) => ({
-            rank: index+1,
-            avatar: user.avatar,
-            nome: user.nome,
-            nivel: user.level,
-            problemasResolvidos: user.NumeroDeproblemasResolvidos,
-        }));
+           const usuarios = await UsuarioModel.find().sort({level:-1, NumeroDeproblemasResolvidos: -1});
+
+           if(!usuarios) return res.status(400).json('usuarios nao encontrados');
+           
+          
+           const ranking = usuarios.map((user, index) => ({
+              rank: index+1,
+              avatar: user.avatar,
+              nome: user.nome,
+              nivel: user.level,
+              problemasResolvidos: user.NumeroDeproblemasResolvidos,
+            }));
 
            return res.status(200).json(ranking)
 
            
-        }
+        
 
 
 
