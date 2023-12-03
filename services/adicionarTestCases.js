ae3a03922c7b5120529d00f7214af0d99788ecba
juuhadg <juku.jj@gmail.com>
2023-12-03 12:56:04 -0300

    Update adicionarTestCases.js

--- services/adicionarTestCases.js
+++ services/adicionarTestCases.js
@@ -17,12 +17,43 @@ export default function adicionarTestCases(dados) {
     const codigo2Buffer = Buffer.from(dados.codigo, 'base64');
     const codigoConvertidoString = codigo2Buffer.toString('utf-8');
 
-        let codigo = 
-        
-            `${comando}(${dados.nomeDaFuncao + `(${dados.testCases[0]})`}); ${comando}(${dados.nomeDaFuncao + `(${dados.testCases[1]})`});  ${comando}(${dados.nomeDaFuncao + `(${dados.testCases[2]})`});`
+        if(dados.linguagem === 'csharp') {
+            dados.nomeDaFuncao = dados.nomeDaFuncao.charAt(0).toUpperCase() + dados.nomeDaFuncao.slice(1);
+
+                var codigo =  `using System;
+
+                namespace Problema
+                {
+                    class ProblemaMain
+                    {
+                        ${codigoConvertidoString}
+                
+                        static void Main()
+                        {
+                            Console.WriteLine(${dados.nomeDaFuncao}(${dados.testCases[0]}));
+                            Console.WriteLine(${dados.nomeDaFuncao}(${dados.testCases[1]}));
+                            Console.WriteLine(${dados.nomeDaFuncao}(${dados.testCases[2]}));
+
+                        }
+                    }
+                }
+                `
+               
+
+
+                
+        }
+            else {
+
+                codigo = 
+               
+                   `
+       ${comando}(${dados.nomeDaFuncao + `(${dados.testCases[0]})`}); ${comando}(${dados.nomeDaFuncao + `(${dados.testCases[1]})`});  ${comando}(${dados.nomeDaFuncao + `(${dados.testCases[2]})`});
+                   `
+                   codigo = codigoConvertidoString + codigo
+            }
            
        
-            codigo = codigoConvertidoString + codigo
 
             const codigoBuffer = Buffer.from(codigo, 'utf-8');
             const codigoConvertidoBase64 = codigoBuffer.toString('base64');
